bd9ae374f1081a4ceba87c0e0611701ee3eb7a99
YUAN <yankees2484@gmail.com>
2020-11-26 07:36:06 +0800

    建立專案，建立資料庫連線

---
+++ prjnorthwindCRUDByJQueryAjax/northwindCRUDByJQueryAjax/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using northwindCRUDByJQueryAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace northwindCRUDByJQueryAjax.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employees> GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ prjnorthwindCRUDByJQueryAjax/northwindCRUDByJQueryAjax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="northwindCRUDByJQueryAjax.MvcApplication" Language="C#" %>
