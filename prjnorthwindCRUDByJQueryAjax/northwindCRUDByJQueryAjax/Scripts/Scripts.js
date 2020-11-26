16fa4e75564434e73914994aceff703cc93c9b0c
YUAN <yankees2484@gmail.com>
2020-11-26 08:36:49 +0800

    圖檔上傳與新增員工資料

--- prjnorthwindCRUDByJQueryAjax/northwindCRUDByJQueryAjax/Scripts/Scripts.js
+++ prjnorthwindCRUDByJQueryAjax/northwindCRUDByJQueryAjax/Scripts/Scripts.js
@@ -1,1 +1,22 @@
-﻿
+﻿//頁面process
+$(function () {
+    $('#loaderbody').addClass('hide');
+
+    $(document).bind('ajaxStart', function () {
+        $('#loaderbody').removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $('#loaderbody').addClass('hide');
+    });
+});
+
+//檢視圖片上傳
+function ShowImagePreview(imagesUploader, previewImage) {
+    if (imagesUploader.files && imagesUploader.files[0]) {
+        //有檔案就讀檔
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imagesUploader.files[0]);
+    }
+}
